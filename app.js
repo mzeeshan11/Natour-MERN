d6bda5f46a875b2d440f7d2fdd89daaa8c3e8b3f
ZeeShan <m.zeeshan1600@gmail.com>
2022-10-24 16:41:31 +0500

    Natours Initial Commit:

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.status(200).json({ hello: 'Hello from the server side', app: 'Natours ' })
+})
+app.post('/', (req, res) => {
+    res.send('You can post to this endpoint...')
+})
+
+const port = 4000
+app.listen(port, () => {
+    console.log(`App running on port ${port}...`)
+})  
